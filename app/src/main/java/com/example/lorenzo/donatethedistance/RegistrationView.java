22cb6a4886888763baeb6b8e160edaae0ee75e19
Lorenzo <renzob.kc@gmail.com>
2016-02-08 10:47:55 -0600

    registration page looks are done

--- app/src/main/java/com/example/lorenzo/donatethedistance/RegistrationView.java
+++ app/src/main/java/com/example/lorenzo/donatethedistance/RegistrationView.java
@@ -1,25 +1,22 @@
 package com.example.lorenzo.donatethedistance;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.NumberPicker;
 
 public class RegistrationView extends AppCompatActivity {
-
-    private Integer[] arraySpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_view);
 
-        this.arraySpinner = new Integer[] {
-                1, 2, 3, 4, 5
-        };
-        Spinner s = (Spinner) findViewById(R.id.spinner);
-        ArrayAdapter<Integer> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_spinner_item, arraySpinner);
-        s.setAdapter(adapter);
+        NumberPicker np = (NumberPicker) findViewById(R.id.np_feet);
+        np.setMaxValue(9);
+        np.setMinValue(1);
+
+        np = (NumberPicker) findViewById(R.id.np_inches);
+        np.setMinValue(0);
+        np.setMaxValue(11);
     }
 }
