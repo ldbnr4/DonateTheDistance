8b6270b09bcde575f25618e6626cf6211d717f0e
Lorenzo <renzob.kc@gmail.com>
2016-03-10 17:05:09 -0600

    able to move from charity selection to activity selection

--- app/src/main/java/com/example/lorenzo/donatethedistance/ActivitySelectionView.java
+++ app/src/main/java/com/example/lorenzo/donatethedistance/ActivitySelectionView.java
@@ -1,7 +1,10 @@
 package com.example.lorenzo.donatethedistance;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ActivitySelectionView extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class ActivitySelectionView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_selection_view);
+
+        //Get message from intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(CharitySelectionView.SELECTED_CHARITY);
+
+        //Create the text view
+        TextView textView = (TextView) findViewById(R.id.charityText);
+        //textView.setTextSize(40);
+        textView.setText(message);
+
+        ImageView imageView = (ImageView) findViewById(R.id.charityImage);
+
+        if (message.equals(getString(R.string.charity_water_name_txt))) {
+            imageView.setImageResource(R.drawable.charity_water);
+        } else if (message.equals(getString(R.string.habitat_name_txt))) {
+            imageView.setImageResource(R.drawable.habitat_for_humanity_small);
+        } else if (message.equals(getString(R.string.humane_name_txt))) {
+            imageView.setImageResource(R.drawable.the_humane_society_small);
+        } else if (message.equals(getString(R.string.stand_up_name_txt))) {
+            imageView.setImageResource(R.drawable.stand_up_to_cancer_small);
+        }
     }
+
+
 }
--- app/src/main/java/com/example/lorenzo/donatethedistance/CharitySelectionView.java
+++ app/src/main/java/com/example/lorenzo/donatethedistance/CharitySelectionView.java
@@ -1,13 +1,43 @@
 package com.example.lorenzo.donatethedistance;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class CharitySelectionView extends AppCompatActivity {
+    public final static String SELECTED_CHARITY = "selected charity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_selection_view);
+
+        TableLayout yourRootLayout = (TableLayout) findViewById(R.id.table);
+        int count = yourRootLayout.getChildCount();
+        for (int i = 0; i < count; i++) {
+            View v = yourRootLayout.getChildAt(i);
+            if (v instanceof TableRow) {
+                final TableRow row = (TableRow) v;
+                row.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int rowCount = row.getChildCount();
+                        for (int r = 0; r < rowCount; r++) {
+                            View v2 = row.getChildAt(r);
+                            if (v2 instanceof TextView) {
+                                Intent intent = new Intent(CharitySelectionView.this, ActivitySelectionView.class);
+                                intent.putExtra(SELECTED_CHARITY, (String) ((TextView) v2).getText());
+                                startActivity(intent);
+                            }
+                        }
+                    }
+                });
+            }
+        }
     }
 }
