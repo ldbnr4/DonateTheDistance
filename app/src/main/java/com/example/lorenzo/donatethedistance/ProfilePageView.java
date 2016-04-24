433f495ca6701e32399d4cb2e62c7b4a51925d82
Brendon Sexe <bks437@mail.missouri.edu>
2016-04-24 15:32:53 -0500

    Profile work

--- app/src/main/java/com/example/lorenzo/donatethedistance/ProfilePageView.java
+++ app/src/main/java/com/example/lorenzo/donatethedistance/ProfilePageView.java
@@ -2,12 +2,29 @@ package com.example.lorenzo.donatethedistance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ProfilePageView extends AppCompatActivity {
 
+    private TextView distance;
+    private TextView money;
+    private TextView percentage;
+    private TextView name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page_view);
+        distance = (TextView) findViewById(R.id.totalDistance);
+        money = (TextView) findViewById(R.id.totalDonated);
+        percentage = (TextView) findViewById(R.id.percentageTraveled);
+        name = (TextView) findViewById(R.id.name);
+        name.setText(R.id.first_name + R.id.last_name);
+        /*
+        distance.setText(R.id.distance);
+        money.setText(R.id.donated);
+        percentage.setText(2680/R.id.distance);
+        set graph to imageView
+         */
+
     }
 }
