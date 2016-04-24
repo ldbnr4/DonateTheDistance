f85e9b4c7f0733ab4c70848766031d3264071c34
Brendon Sexe <bks437@mail.missouri.edu>
2016-04-24 00:08:14 -0500

    First block of pages upload
    Selection, detail, sending info to get walk, run, or bike on workout
    page instead of 3 separate pages for walk run and bike

--- app/src/main/java/com/example/lorenzo/donatethedistance/ActivitySelectionView.java
+++ app/src/main/java/com/example/lorenzo/donatethedistance/ActivitySelectionView.java
@@ -1,13 +1,40 @@
 package com.example.lorenzo.donatethedistance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivitySelectionView extends AppCompatActivity {
 
+    String charity = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_selection_view);
     }
+
+    public void sendWalk(View view) {
+        Intent intent1 = new Intent(this, WorkoutView.class);
+        intent1.putExtra("Workout", "Walk");
+        startActivity(intent1);
+    }
+    public void sendRun(View view) {
+        Intent intent2 = new Intent(this, WorkoutView.class);
+        intent2.putExtra("Workout", "Run");
+        startActivity(intent2);
+    }
+    public void sendBike(View view) {
+        Intent intent3 = new Intent(this, WorkoutView.class);
+        intent3.putExtra("Workout", "Bike");
+        startActivity(intent3);
+    }
+
+    public void sendCharity(View view) {
+        Intent intent4 = new Intent(this, CharityDetailView.class);
+        intent4.putExtra("Charity", charity);
+        startActivity(intent4);
+    }
+
 }
--- app/src/main/java/com/example/lorenzo/donatethedistance/WorkoutView.java
+++ app/src/main/java/com/example/lorenzo/donatethedistance/WorkoutView.java
@@ -2,6 +2,7 @@ package com.example.lorenzo.donatethedistance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
 
 public class WorkoutView extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class WorkoutView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_view);
+        Intent intent = getIntent();
+        String workout = intent.getStringExtra("Workout");
+        System.out.println(workout);
     }
 }
