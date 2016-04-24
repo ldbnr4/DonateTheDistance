2a852e81f0aebfe99a2c3c79696c18dd8c5fd976
Brendon Sexe <bks437@mail.missouri.edu>
2016-04-24 01:00:55 -0500

    Walking update
    pedometer code

--- app/src/main/java/com/example/lorenzo/donatethedistance/WorkoutView.java
+++ app/src/main/java/com/example/lorenzo/donatethedistance/WorkoutView.java
@@ -1,10 +1,27 @@
 package com.example.lorenzo.donatethedistance;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
 
-public class WorkoutView extends AppCompatActivity {
+public class WorkoutView extends AppCompatActivity implements SensorEventListener{
+
+    private TextView textView;
+
+    private SensorManager mSensorManager;
+
+    private Sensor mStepCounterSensor;
+
+    private Sensor mStepDetectorSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +30,55 @@ public class WorkoutView extends AppCompatActivity {
         Intent intent = getIntent();
         String workout = intent.getStringExtra("Workout");
         System.out.println(workout);
+        textView = (TextView) findViewById(R.id.steps);
+
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        float[] values = event.values;
+        int value = -1;
+
+        if (values.length > 0) {
+            value = (int) values[0];
+        }
+
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            textView.setText("Step Counter Detected : " + value);
+        } else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            // For test only. Only allowed value is 1.0 i.e. for step taken
+            textView.setText("Step Detector Detected : " + value);
+        }
+    }
+
+    protected void onResume() {
+
+        super.onResume();
+
+        mSensorManager.registerListener(this, mStepCounterSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mStepDetectorSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mStepDetectorSensor);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
